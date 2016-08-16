25b8f6f5b9f82ce3c9b9939fefecaa618113e583
kellerkindt <michael@kellerkindt.com>
2016-08-16 22:48:38 +0200

    Hello GitHub
     - updated copyright header in all source files
     - added README.md and LICENSE file

---
+++ README.md
@@ -0,0 +1,15 @@
+# ShowCaseStandalone
+
+A Minecraft-Bukkit-API Shop Plugin
+
+[Bukkit Project Page](http://dev.bukkit.org/bukkit-plugins/scs/)
+
+## License
+This project is licensed under [GPLv3](LICENSE) 
+
+
+### Libraries / Dependencies
+Binaries and other files located at lib/ are plugins and/or projects
+of other authors. The license might differ. They are only included for
+the java compiler to be able to resolve the dependencies and are not
+included in the compiled output file.
--- src/com/kellerkindt/scs/interfaces/Balance.java
+++ src/com/kellerkindt/scs/interfaces/Balance.java
@@ -1,20 +1,20 @@
-/**
-* ShowCaseStandalone
-* Copyright (C) 2012 Kellerkindt <copyright at kellerkindt.com>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * ShowCaseStandalone - A Minecraft-Bukkit-API Shop Plugin
+ * Copyright (C) 2016-08-16 22:43 +02 kellerkindt (Michael Watzko) <copyright at kellerkindt.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.kellerkindt.scs.interfaces;
 
 import com.kellerkindt.scs.internals.NamedUUID;
