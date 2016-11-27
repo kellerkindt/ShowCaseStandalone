99cd7ce4951bd73361f8c099d49d3c144eadd366
RoboMWM <robomwm@users.noreply.github.com>
2016-11-26 22:18:15 -0800

    ugh

--- src/com/kellerkindt/scs/internals/SimpleShopHandler.java
+++ src/com/kellerkindt/scs/internals/SimpleShopHandler.java
@@ -759,7 +759,7 @@ public class SimpleShopHandler implements ShopHandler, Listener {
         // TODO experimental
         if (scs.getConfiguration().isCustomNameVisible()) {
             String text = scs.getConfiguration().customNameFormat();
-            String itemName = itemStack.getType().toString();
+            String itemName = itemStack.getType().name();
             if (meta != null)
                 itemName = meta.getDisplayName();
             else if (item.getName() != null)
